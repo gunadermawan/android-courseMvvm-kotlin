b0e1d9eff56eaa6439d3ffca115ba19288e7aa58
gunadermawan <gunadermawan62@gmail.com>
2022-06-22 05:53:04 +0700

    listener utk tiap btn

--- app/src/main/java/com/gunder/coursemvvm/MainActivity.kt
+++ app/src/main/java/com/gunder/coursemvvm/MainActivity.kt
@@ -1,11 +1,70 @@
 package com.gunder.coursemvvm
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.gunder.coursemvvm.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var activityMainBinding: ActivityMainBinding
+    private var scoreA = 0
+    private var scoreB = 0
     override fun onCreate(savedInstanceState: Bundle?) {
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(activityMainBinding.root)
+        supportActionBar?.hide()
+        activityMainBinding.btnPlusScoreTeamA.setOnClickListener(this)
+        activityMainBinding.btnMinusScoreTeamA.setOnClickListener(this)
+        activityMainBinding.btnPlusScoreTeamB.setOnClickListener(this)
+        activityMainBinding.btnMinusScoreTeamB.setOnClickListener(this)
+        activityMainBinding.btnReset.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btn_plus_score_team_a -> {
+                addScoreA()
+            }
+            R.id.btn_minus_score_team_a -> {
+                minScoreA()
+            }
+            R.id.btn_plus_score_team_b -> {
+                addScoreB()
+            }
+            R.id.btn_minus_score_team_b -> {
+                minScoreB()
+            }
+            R.id.btn_reset -> {
+                resetScore()
+            }
+        }
+    }
+
+    private fun resetScore() {
+        scoreA = 0
+        scoreB = 0
+        activityMainBinding.tvCardTeamA.text = scoreA.toString()
+        activityMainBinding.tvCardTeamB.text = scoreB.toString()
+    }
+
+    private fun minScoreB() {
+        scoreB -= 1
+        activityMainBinding.tvCardTeamB.text = scoreB.toString()
+    }
+
+    private fun addScoreB() {
+        scoreB += 1
+        activityMainBinding.tvCardTeamB.text = scoreB.toString()
+    }
+
+    private fun minScoreA() {
+        scoreA -= 1
+        activityMainBinding.tvCardTeamA.text = scoreA.toString()
+    }
+
+    private fun addScoreA() {
+        scoreA += 1
+        activityMainBinding.tvCardTeamA.text = scoreA.toString()
     }
 }

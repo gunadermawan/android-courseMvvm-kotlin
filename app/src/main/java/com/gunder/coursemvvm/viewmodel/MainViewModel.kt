80013a123edc6f4b6bde4e3f29b1b68603c63c92
gunadermawan <gunadermawan62@gmail.com>
2022-06-22 11:03:45 +0700

    create fun in MainViewModel

--- app/src/main/java/com/gunder/coursemvvm/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/gunder/coursemvvm/viewmodel/MainViewModel.kt
@@ -9,26 +9,43 @@ class MainViewModel : ViewModel() {
     private val scoreB: MutableLiveData<Int> = MutableLiveData()
 
     fun resetScore() {
-
+        scoreA.value = 0
+        scoreB.value = 0
     }
 
     fun minScoreB() {
-
+        val result = getScoreB().value?.minus(1)
+        if (result != null) {
+            if (result < 0) {
+                scoreB.value = 0
+            } else {
+                scoreB.value = result
+            }
+        }
     }
 
     fun addScoreB() {
-
+        val result = getScoreB().value?.plus(1)
+        scoreB.value = result
     }
 
     fun minScoreA() {
-
+        val result = getScoreA().value?.minus(1)
+        if (result != null) {
+            if (result < 0) {
+                scoreA.value = 0
+            } else {
+                scoreA.value = result
+            }
+        }
     }
 
     fun addScoreA() {
-
+        val result = getScoreA().value?.plus(1)
+        scoreA.value = result
     }
 
-    fun getScoreA(): LiveData<Int> {
+    private fun getScoreA(): LiveData<Int> {
         if (scoreA.value == null) {
             scoreA.value = 0
         }
